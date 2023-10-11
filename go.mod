e5aa89de3437fdcdaf23b6f03327acd2e20d6f12
mikanfactory <k952i4j14x17@gmail.com>
2023-10-11 17:15:17 +0900

    impl parser

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mikanfactory/monkey
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"github.com/mikanfactory/monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	usr, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		usr.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
